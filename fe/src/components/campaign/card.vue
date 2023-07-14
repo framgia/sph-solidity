5cfaddcedc0640b5f8830230ec216091090c0ef9
John Paul Banera <johnpaul.banera@sun-asterisk.com>
2023-07-14 16:05:17 +0800

    SOL-67 [Integrate] Integrate smart contract function for viewing recent campaigns in landing page

--- fe/src/components/campaign/card.vue
+++ fe/src/components/campaign/card.vue
@@ -1,25 +1,27 @@
 <template>
   <div
-    class="rounded-2xl overflow-hidden bg-white shadow-card"
+    class="rounded-2xl overflow-hidden bg-white shadow-card flex flex-col justify-between"
     :class="additionalClass ?? ''"
   >
-    <img :src="imgSrc" class="h-44 w-full object-cover" />
-    <div class="m-4 leading-[150%]">
-      <div class="text-dark text-base font-bold">{{ title }}</div>
-      <div class="mt-2 text-disabled text-sm font-normal flex items-center">
-        <p class="text-ellipsis overflow-hidden ...">
-          {{ truncate(description, 95) }}
-        </p>
+    <div>
+      <img :src="imgSrc" class="h-44 w-full object-cover" />
+      <div class="m-4 leading-[150%]">
+        <div class="text-dark text-base font-bold">{{ title }}</div>
+        <div class="mt-2 text-disabled text-sm font-normal flex items-center">
+          <p class="text-ellipsis overflow-hidden ...">
+            {{ truncate(description, 95) }}
+          </p>
+        </div>
       </div>
-      <div class="mt-6">
-        <div class="font-bold text-dark flex justify-between">
-          <div class="text-sm">{{ ethValue }}</div>
-          <div class="text-base w-4/12 text-left">{{ daysLeft }}</div>
-        </div>
-        <div class="font-normal text-primary-500 flex text-sm justify-between">
-          <div>Raised ETH</div>
-          <div class="w-4/12 text-left">Days Left</div>
-        </div>
+    </div>
+    <div class="mx-4 mb-4">
+      <div class="font-bold text-dark flex justify-between">
+        <div class="text-sm">{{ ethValue }}</div>
+        <div class="text-base w-4/12 text-left">{{ daysLeft }}</div>
+      </div>
+      <div class="font-normal text-primary-500 flex text-sm justify-between">
+        <div>Raised ETH</div>
+        <div class="w-4/12 text-left">Days Left</div>
       </div>
     </div>
   </div>
--- fe/src/plugins/ethers.ts
+++ fe/src/plugins/ethers.ts
@@ -6,7 +6,7 @@ import { useWalletStore } from "~/store/wallet";
 
 export default defineNuxtPlugin(async () => {
   const { isConnected } = storeToRefs(useWalletStore());
-  const CONTRACT_ADDRESS = "0x98668C834ca6A0317bC94702fcf589D5bCFb1Fa5";
+  const CONTRACT_ADDRESS = "0x402BFe8c29aC35947416a94b42de8e87CE20FAB4";
 
   const ethereum = window.ethereum;
   const provider = new ethers.BrowserProvider(ethereum);
@@ -26,7 +26,13 @@ export default defineNuxtPlugin(async () => {
 
         return smartContract;
       } catch (error) {
-        toast.error("Something went wrong!");
+        const walletConnectionError = "UNKNOWN_ERROR";
+
+        if ((error as { code: string }).code === walletConnectionError) {
+          toast.error("Wallet connection failed!");
+        } else {
+          toast.error("Something went wrong!");
+        }
         return null;
       }
     } else {
